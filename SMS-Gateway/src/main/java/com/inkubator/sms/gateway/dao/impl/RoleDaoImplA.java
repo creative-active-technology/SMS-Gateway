2f303dab41d4fd030dc0b44b071ba41f9e57cca0
EKA <EKA@Positron>
2015-01-14 13:20:24 +0700

    rename dao & service : role

--- SMS-Gateway/src/main/java/com/inkubator/sms/gateway/dao/impl/RoleDaoImplA.java
+++ SMS-Gateway/src/main/java/com/inkubator/sms/gateway/dao/impl/RoleDaoImplA.java
@@ -6,7 +6,7 @@
 package com.inkubator.sms.gateway.dao.impl;
 
 import com.inkubator.datacore.dao.impl.IDAOImpl;
-import com.inkubator.sms.gateway.dao.RoleDao;
+import com.inkubator.sms.gateway.dao.RoleDaoA;
 import com.inkubator.sms.gateway.entity.Role;
 import java.util.List;
 import org.apache.lucene.search.Query;
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository(value = "roleDao")
 @Lazy
-public class RoleDaoImpl extends IDAOImpl<Role> implements RoleDao{
+public class RoleDaoImplA extends IDAOImpl<Role> implements RoleDaoA{
     private static final long serialVersionUID = -3838059330383642763L;
 
     @Override
@@ -71,4 +71,5 @@ public class RoleDaoImpl extends IDAOImpl<Role> implements RoleDao{
         FullTextQuery fullTextQuery = fullTextSession.createFullTextQuery(luceneQuery, getEntityClass());
         return fullTextQuery;
     }
+    
 }
